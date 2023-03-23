ab14e5f8b9070382da477793cce1d4c389c41221
Guled <guletkadar@gmail.com>
2023-03-24 01:11:12 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const divide = function (){
+    let a = 2000;
+    let b = 100;
+    return a / b;
+}
+
+console.log(divide());
+
+let x = 2;
+const square = ( u =>  x * 2)  ;
+console.log(square());
+
+let a = 3;
+let b = 4;
+const add = ( u =>  a+b)  ;
+console.log(add());
+
